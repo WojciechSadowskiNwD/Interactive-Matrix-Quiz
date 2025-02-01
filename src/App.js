fe194b0e841131d3aaf6b8532ed1e6d73b13f8c8
Wojciech Sadowski <wojciechnwd@gmail.com>
2025-02-01 18:52:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MatrixRain from "./components/canvas/MatrixRain";
+import "./App.css";
+
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <MatrixRain />
+        </div>
+    );
+};
+
+export default App;
