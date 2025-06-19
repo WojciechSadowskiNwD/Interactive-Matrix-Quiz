e414b1c115b14ad55fdbe5e1fb704580c9e5f248
Wojciech Sadowski <wojciechnwd@gmail.com>
2025-06-19 17:21:59 +0200

    A new WelcomeImg component was created to appear for a moment and disappear. AnimatePresence from the framer-motion library was carved out for this.

---
+++ src/components/WelcomeImg.js
@@ -0,0 +1,31 @@
+import {motion} from "framer-motion";
+import styles from "./WelcomeImg.module.scss";
+
+export default function WelcomeImg() {
+	return (
+		<motion.div className={styles.welcomeImg_wrapper}
+            initial={{opacity: 1}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 2.5}}
+        >
+			<div className={styles.img_box}>
+				<img
+					className={styles.image}
+					src="img/openApp.webp"
+					alt="Man in black coat holding two pills in open hands"
+				/>
+                <motion.em className={styles.text}
+                    initial={{opacity: .2}}
+                    animate={{opacity: [.3,1,.3]}}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        ease: "easeInOut"
+                    }}
+
+                >Initialize</motion.em>
+			</div>
+		</motion.div>
+	);
+}
--- src/pages/StartApp.js
+++ src/pages/StartApp.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence } from "framer-motion";
 import { toggleActive } from "../store/cursorSlice";
-import BlackBoard from "../components/BlackBoard";
+import WelcomeImg from "../components/WelcomeImg";
 import MatrixRain from "../components/canvas/MatrixRain";
+import BlackBoard from "../components/BlackBoard";
 import CustomCursor from "../components/CustomCursor";
 
 // To będzie tło dla ekranu startowego oraz 3 pozostałych pages poza EnterTheQuiz
@@ -11,24 +13,38 @@ function StartApp() {
 	const dispatch = useDispatch();
 	const { activeCursor } = useSelector((store) => store.cursor);
 	const { firstLaunch, activeComponent } = useSelector((store) => store.ui);
-	
-	
+	const [showWelcome, setShowWelcome] = useState(true);
+	const [startFadeOut, setStartFadeOut] = useState(false);
+
+	useEffect(() => {
+		const fadeDelay = setTimeout(() => setStartFadeOut(true), 1500);
+		const hideDelay = setTimeout(() => setShowWelcome(false), 2500);
+
+		return () => {
+			clearTimeout(fadeDelay);
+			clearTimeout(hideDelay);
+		};
+	}, []);
+
 	useEffect(() => {
 		const timeTriggers = [13200, 1000, 5000];
 		let timeout = null;
-		
-		if(firstLaunch){
+
+		if (firstLaunch) {
 			setStep(0);
-		} else if(activeComponent === "scoreBoard" || activeComponent === "aboutApp" || activeComponent === "aboutDev"){
+		} else if (
+			activeComponent === "scoreBoard" ||
+			activeComponent === "aboutApp" ||
+			activeComponent === "aboutDev"
+		) {
 			setStep(1);
-		}else if(activeComponent === "options"){
+		} else if (activeComponent === "options") {
 			setStep(2);
 		}
 
-		if(!activeCursor){
-				timeout = setTimeout(() => dispatch(toggleActive()), timeTriggers[step]);
+		if (!activeCursor) {
+			timeout = setTimeout(() => dispatch(toggleActive()), timeTriggers[step]);
 		}
-
 
 		return () => clearTimeout(timeout);
 	}, [dispatch, activeCursor, firstLaunch, step, activeComponent]);
@@ -36,6 +52,10 @@ function StartApp() {
 	return (
 		<>
 			<MatrixRain />
+			<AnimatePresence>
+				{showWelcome && <WelcomeImg key={startFadeOut ? "exit" : "enter"} />}
+			</AnimatePresence>
+
 			<BlackBoard />
 			{activeCursor && <CustomCursor />}
 		</>
